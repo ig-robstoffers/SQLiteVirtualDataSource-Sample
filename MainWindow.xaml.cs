00cd6df6abfd28e668e7a4873b2fe15591f40135
Robert Stoffers <rstoffers@infragistics.com>
2020-04-28 13:15:52 -0400

    add db file and hook it up to a grid

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,5 +1,8 @@
-﻿using System;
+﻿using Infragistics.Controls.DataSource;
+using SQLite;
+using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -18,11 +21,68 @@ namespace SQLiteVirtualDataSource_Sample
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class MainWindow : Window, INotifyPropertyChanged
     {
+        #region INotifyPropertyChanged Implementation
+        public event PropertyChangedEventHandler PropertyChanged;
+        protected void OnPropertyChanged(string propertyName)
+        {
+            if (PropertyChanged != null)
+                PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+        }
+        #endregion
+
+        private SQLiteVirtualDataSource _dataSource;
+        public SQLiteVirtualDataSource DataSource
+        {
+            get { return _dataSource; }
+            set
+            {
+                _dataSource = value;
+                OnPropertyChanged("DataSource");
+            }
+        }
+
         public MainWindow()
         {
             InitializeComponent();
+            DataContext = this;
+
+            var connection = new SQLiteAsyncConnection("chinook.db");
+
+            var dataSource = new SQLiteVirtualDataSource();
+            dataSource.Connection = connection;
+            dataSource.TableExpression = "tracks left outer join albums on tracks.AlbumId = albums.AlbumId";
+            dataSource.ProjectionType = typeof(Track);
+            DataSource = dataSource;
         }
+    }
+
+    [Table("tracks")]
+    public class Track
+    {
+        [PrimaryKey, AutoIncrement]
+        public int TrackId { get; set; }
+
+        [MaxLength(200)]
+        public string Name { get; set; }
+
+        public int AlbumId { get; set; }
+
+        [Column("Title")]
+        public string AlbumTitle { get; set; }
+
+        public int MediaTypeId { get; set; }
+
+        public int GenreId { get; set; }
+
+        [MaxLength(220)]
+        public string Composer { get; set; }
+
+        public int Milliseconds { get; set; }
+
+        public int Bytes { get; set; }
+
+        public decimal UnitPrice { get; set; }
     }
 }
